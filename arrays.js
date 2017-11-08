86849138c2c79705c3ad4c07a78139b948d62cdd
Eric Rogers <eric.rog17@gmail.com>
2017-11-08 22:30:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(arr) {
 }
 
 function removeElementFromEndOfArray(arr) {
-  arr.slice(0, arr.length - 1);
-  return arr;
+  var arrayWithLastElementRemoved = arr.slice(0, arr.length - 1);
+  return arrayWithLastElementRemoved;
 }
